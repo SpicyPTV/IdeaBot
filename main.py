4009cfda0b88da8e05c958549761e537feb65b6d
Pieter Edwards <PieterEd12@Gmail.com>
2021-12-21 01:50:21 +0000

    Added comments and expanded the amount of ideas

--- main.py
+++ main.py
@@ -5,23 +5,28 @@ from numpy import random
 
 client = commands.Bot(command_prefix="!")
 token = os.environ['token']
-ideas = ['Decorate the server', 'Add a welcome bot', 'Add an admin bot', 'Run a competition', 'Add new emotes', 'Invite new people to the server', 'Hope on voice chat', 'Share a project you\'re working on', ]
+ideas = ['Decorate the server', 'Add a welcome bot', 'Add an admin bot', 'Run a competition', 'Add new emotes', 'Invite new people to the server', 'Hop on voice chat', 'Share a project you\'re working on', 'Start a new project', 'Set out a roadmap for your current project', 'Setup a server wide']
 
+# Makes sure the bot loads
 
 @client.event
 async def on_ready():
     print("ready")
+    print(len(ideas)) # Debug
 
+# Basic intro to the Bot's commands
 
 @client.command()
 async def hello(ctx):
-    await ctx.send("Hello, I am your bot!")
+    await ctx.send("Idea Bot is ready and waiting, just type !idea for an idea in one of your text channels!')
 
+# Command that prints the idea
 
 @client.command()
 async def idea(ctx):
-  rand = random.randint(7)
+  rand = random.randint(11)
   await ctx.send(ideas[rand])
 
+# Authorizes and runs the bot
 
 client.run(token)
